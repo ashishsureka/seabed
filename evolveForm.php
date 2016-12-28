73f440536f61228116116b08a967d01aacf4bc9e
Veena Saini <veena.sainiaug2shine@gmail.com>
2016-12-28 20:36:42 +0530

    This file handles actions of pop up form in Case Evolution field

---
+++ evolveForm.php
@@ -0,0 +1,52 @@
+<?php 
+session_start();
+
+$_SESSION['captcha']=0;
+if(isset($_POST['g-recaptcha-response'])&&!empty($_POST['g-recaptcha-response']))
+{
+    
+    $secret="6LfocQ4UAAAAAMjnIA3De1ZMLqBVHRpqLHsmUCx7";
+    $ip=$_SERVER['REMOTE_ADDR'];
+    $captcha=$_POST['g-recaptcha-response'];
+
+    $rsp=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$captcha&remoteip=$ip");
+     
+    
+   $arr=json_decode($rsp,TRUE);
+
+     if($arr['success'])
+      {
+         //echo 'success';
+      	$_SESSION['captcha']=1;
+      }
+     else
+      die('Captcha mismatch. <a href="'.$http_referer.'">Please try again </a>');
+
+}
+
+
+
+
+
+
+
+
+
+
+$try=$_POST['boxes2'];
+$_SESSION['try']=$try;
+
+ $_SESSION['author_details']= serialize( $_POST['boxes2'] );
+
+
+$_SESSION['date']=$_POST['date'];
+
+
+if(isset($_POST['notification'])&&!empty($_POST['notification']))
+ $notify="ON";
+else
+$notify="OFF";
+
+$_SESSION['notify']=$notify;
+
+?>
